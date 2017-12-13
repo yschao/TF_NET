51d743e6868946b47f13468473d1040ea75cfb4f
TiRan_Yang <songchao.yang@tcl.com>
2017-12-13 10:19:27 +0800

    1 add get train label

--- data_convert.py
+++ data_convert.py
@@ -15,15 +15,30 @@ def untar_train_data():
         os.system('tar -xvf %s -C %s' %(sub_dir, train_dir_name))
 
 def get_train_file_name():
-    train_labels = os.listdir(os.path.join(PATH, 'train_data'))
+    train_labels = get_train_label()
+    train_names = os.listdir(os.path.join(PATH, 'train_data'))
     with open('train_name_paths.txt', 'w') as f:
-        for train_label in train_labels:
-            label_path = os.path.join(os.path.join(PATH, 'train_data'), train_label)
+        for train_name in train_names:
+            label = train_labels.index(train_name)
+            label_path = os.path.join(os.path.join(PATH, 'train_data'), train_name)
             lable_names = os.listdir(label_path)
             for label_name in lable_names:
                 img_path = os.path.join(label_path, label_name)
-                f.write(img_path + '\n')
+                f.write(img_path + ' ' + str(label) + '\n')
+
 
 def get_train_label():
+    path = 'synset_words.txt'
+    train_labels = []
+    f = open(path, 'r')
+    labels = f.readlines()
+    f.close()
+    for i, label in enumerate(labels):
+        train_labels.append(label.split(' ')[0])
+    return train_labels
 
-untar_train_data()
+
+if __name__ == '__main__':
+    # untar_train_data()
+    get_train_label()
+    exit(0)
